c452a80985b7f51a5c3556b67c9372c11c4ca20b
teetoye <akintoye.asaolu@yahoo.com>
2020-04-15 21:57:29 -0500

    final

--- Quiz_II.py
+++ Quiz_II.py
@@ -10,7 +10,7 @@ for i, j in Animal_data_df.iterrows():
     for k in (j[3].split(',')):
         l = k.strip()
         Animal_dict[l] = (j[0], j[2])
-class_details = {i: j for i, j in Animal_dict.values()} #dictiomnary comprehnsion
+class_details = {i: j for i, j in Animal_dict.values()} #dictionary comprehnsion
 #This next python class module is not needed I could have worked through dictionaries but I feel I need to take every oportunity to practice python classes. I struggled with them while working through my Py-game project
 class Animal:
     def __init__(self, name):
@@ -27,7 +27,7 @@ x_train, y_train = Animal_data_train[0:101, 0:16],  (Animal_data_train[0:101, 16
 #--Read Test Data
 Animal_data_test = pd.read_csv('animals_test.csv')
 animals = Animal_data_test.animal_name
-class_numb = [(Animal(i)).class_number for i in animals]
+class_numb = [(Animal(i)).class_number for i in animals]#list comprehension
 y_test = (pd.Series(class_numb)).to_numpy()
 x_test = (Animal_data_test.drop(['animal_name'], axis=1)).to_numpy()
 #Create Model- Train the model
@@ -37,9 +37,9 @@ knn.fit(X=x_train, y=y_train)
 predicted = knn.predict(X=x_test)
 expected = y_test #These are the expected class_numbers
 #get the animal name for these class_numbers
-predicted_class_list = [Animal(animals[i]).return_class_type(i) for i in predicted]
+predicted_class_list = [Animal(animals[i]).return_class_type(i) for i in predicted]#list comprehension
 #Create a dataframe that has the animal name and the predicted class_type
-prediction = pd.Series(predicted_class_list, name="prediction") #cobnverts the predicted class_type to a panda series
+prediction = pd.Series(predicted_class_list, name="prediction") #converts the predicted class_type to a panda series
 Results = pd.concat([animals, prediction], axis=1) #concatenate the prediction panda series with the animals series to get a result data frame.
 #write result data frame to CSV FILE
 Results.to_csv('predictions.csv', sep=',', index=False)
